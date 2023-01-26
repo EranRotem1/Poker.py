77ef1b7db113369db96781cdb9d1d4595bd3ee51
Eran Rotem <eranrotem123@gmail.com>
2023-01-25 23:07:37 -0600

    deck class + card class method

--- Poker/TexasHoldem.py
+++ Poker/TexasHoldem.py
@@ -9,10 +9,37 @@ class Card():
             raise ValueError(f"suit must be: {self.SUITS}")
         self.rank = rank
         self.suit = suit
+    
+    @classmethod
+    def make_52_card_deck(cls):
+        return [
+            cls(rank, suit)
+            for rank in cls.RANKS 
+            for suit in cls.SUITS
+        ]
 
-    def __str__(self):
+    def __repr__(self):
         return f"{self.rank} of {self.suit}"
 
-print(Card("2", "Hearts"))
-print(Card("Ace", "Spades"))
+    # def __repr__(self):
+    #     return f"Card({self.rank},{self.suit})"
 
+# print(Card("2", "Hearts"))
+# print(Card("Ace", "Spades"))
+
+class Deck():
+    def __init__(self):
+        self.cards = []
+
+    def __str__(self):
+        return f"{self.cards}"
+
+    def add_cards(self, cards):
+        self.cards.extend(cards)
+
+
+
+# standard_deck = Card.make_52_card_deck()
+# deck = Deck()
+# deck.add_cards(standard_deck)
+# print(deck)
